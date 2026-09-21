2e17612f6a77f1f4fad566d88d14c34b094e4d25
Luan Verdelho <luanverdelho642@gmail.com>
2026-09-21 13:55:08 -0300

    feat: create Lunar Wolf JetBrains theme

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("org.jetbrains.intellij.platform") version "2.19.0"
+}
+
+group = "dev.luanverdelho"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdea("2026.2.3")
+    }
+}
